886245bdd5bde44c6a620443158af37441112feb
Jonathan Muraña <jmurana@fing.edu.uy>
2020-07-16 17:48:45 -0300

    stats

--- dras-controller/run_scenario.sh
+++ dras-controller/run_scenario.sh
@@ -1,5 +1,5 @@
-#mvn exec:java -Dexec.mainClass="uy.udelar.fing.dras.DatacenterControllerDiscrete" -Dexec.args="instances/"$1" "$2" 4 0.5 lowest-id-first "$3" "$4
+mvn exec:java -Dexec.mainClass="uy.udelar.fing.dras.DatacenterControllerDiscrete" -Dexec.args="instances/"$1" "$2" 4 0.5 lowest-id-first "$3" "$4
 mvn exec:java -Dexec.mainClass="uy.udelar.fing.dras.DatacenterControllerDiscrete" -Dexec.args="instances/"$1" "$2" 4 0.5 PL "$3" "$4
-#mvn exec:java -Dexec.mainClass="uy.udelar.fing.dras.DatacenterControllerDiscrete" -Dexec.args="instances/"$1" "$2" 4 0.5 PD "$3" "$4
-#mvn exec:java -Dexec.mainClass="uy.udelar.fing.dras.DatacenterControllerDiscrete" -Dexec.args="instances/"$1" "$2" 4 0.5 DL "$3" "$4 
+mvn exec:java -Dexec.mainClass="uy.udelar.fing.dras.DatacenterControllerDiscrete" -Dexec.args="instances/"$1" "$2" 4 0.5 PD "$3" "$4
+mvn exec:java -Dexec.mainClass="uy.udelar.fing.dras.DatacenterControllerDiscrete" -Dexec.args="instances/"$1" "$2" 4 0.5 DL "$3" "$4 
 
